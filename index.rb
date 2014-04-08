820a3b42067ecdef5f3fb7580c31375e55ca7907
Brandon Burnett <gentoolicious@gmail.com>
2014-04-08 20:49:33 +0000

    rubysnoop will now scan when supplied a single host

--- index.rb
+++ index.rb
@@ -1,5 +1,8 @@
 require 'sinatra'
 require 'erb'
+require 'nmap/program'
+require 'nmap/xml'
+require 'pry'
 
 set :bind, '0.0.0.0'
 
@@ -9,5 +12,29 @@ get '/index' do
 end
 
 post '/scan' do
-  "You wanted to scan '#{params[:address]}'."
+  @host = {}
+  @host['address'] = params[:address] 
+  @host['ports'] = []
+   
+  Nmap::Program.scan do |nmap|
+    nmap.syn_scan = true
+    nmap.service_scan = true
+    nmap.os_fingerprint = true
+    nmap.xml = 'scan.xml'
+    nmap.verbose = true
+
+    nmap.ports = [80, 8080]
+    nmap.targets = params[:address]
+  end
+
+  Nmap::XML.new('scan.xml') do |xml|
+    xml.each_host do |host|
+      "[#{host.ip}]"
+      
+      host.each_port do |port|
+        @host['ports'].push(port)
+      end
+    end
+  end
+  erb :results
 end
